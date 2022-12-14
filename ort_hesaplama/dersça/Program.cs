0de5d528feccb8153f3c0c50132c7d4c1b0aec49
Altay-Akyurek <120604586+Altay-Akyurek@users.noreply.github.com>
2022-12-15 02:06:02 +0300

    Add files via upload

---
+++ ort_hesaplama/dersça/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dersça
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("\n \t\tSınav not ortalaması ve geçme kalma durumu belirleme programı\n\n");
+            Console.Write("Lütfen vize notunuzu giriniz:");
+            int vize=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Lütfen vize ağırlığını giriniz notunuzu giriniz:");
+            int vizea = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Lütfen ödev notunuzu giriniz:");
+            int ödev = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Lütfen ödev ağırlığını giriniz notunuzu giriniz:");
+            int ödeva = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Lütfen final notunuzu giriniz:");
+            int final = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Lütfen final ağırlığını giriniz notunuzu giriniz:");
+            int finala = Convert.ToInt32(Console.ReadLine());
+            double ort = (vize * vizea + ödev * ödeva + final * finala) / 100.0;
+            string harf = "FF";
+            if (ort < 45 || final < 50)
+                harf = "FF";
+            else if (ort < 49)
+                harf = "DD";
+            else if (ort < 54)
+                harf = "DC";
+            else if (ort < 57)
+                harf = "CC";
+            else if (ort < 61)
+                harf = "CB";
+            else if (ort < 69)
+                harf = "BB";
+            else if (ort < 76)
+                harf = "BA";
+            else if (ort < 84)
+                harf = "AB";
+            else
+                harf = "AA";
+            Console.WriteLine(harf);
+           
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ort_hesaplama/dersça/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("dersça")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dersça")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("bb370b3f-cbcd-4c12-91e4-b88f621c096c")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
